1b7373ba69a1fbd074a34dd8764b776432a2b8f7
Amanda Spitzner <diabledoux@gmail.com>
2023-06-04 19:28:17 -0300

    divided js into language files

--- js/carousel.js
+++ js/carousel.js
@@ -1,48 +1,29 @@
-const images = [
-  {
-    issueNumber: 001,
-    issueTitle: "Back",
-    url: "0001.png",
-    text: "Yeap. We're back.  Its been a long time and much has changed. Throughout the past few years, I must have thought about Exploding Comics every single day. But it was last week when I was browsing TikTok, that a person I knew from the Dublin conventions commented saying he still missed my comics, that a fire started inside of me, and I thought: What the heck am I doing with my life, if not creating comics? This is part of what I have been since I was like 4. So I decided to combine the fact that I've been passionately studying software development, with the fact that I am a cartoonist and put EC back on track. If you are an old-time reader, I should tell you that everything is quite different, but as I noticed over the couple of storyboards I sketched, my sense of humor and punchline style have been pretty much the same. Can you expect more of the same? Yes. Also, my comics have always been 95% biographical, unless someone was writing the script for me. So I might have to tell you about a lot of not-so-funny situations and major changes I've been through. Still, if you're into simple, nonchalant small stories, this is your place. I missed you guys. A lot. One last thing: I put this whole text inside of a string in Javascript so the text won't have paragraphs 'till tomorrow. xD",
-  },
-  {
-    issueNumber: 002,
-    issueTitle: "Work and Stuff",
-    url: "0002.png",
-    text: "Bro the pandemic hit us hard.",
-  },
-  {
-    issueNumber: 003,
-    issueTitle: "The Curse",
-    url: "0003.png",
-    text: "I am this hypocrite we often talk about in social media.",
-  },
-  {
-    issueNumber: 004,
-    issueTitle: "A New Guy",
-    url: "0004.png",
-    text: "Oh, by the way, we have a new character, in the comic and in my life. We will eventually address that.",
-  },
-  {
-    issueNumber: 005,
-    issueTitle: "Mad Dawg",
-    url: "0005.png",
-    text: "Good morning people. I boldly put a bit of an archive of comics to get the page going, which will make me regret and curse once I run out of material to publish. From now on, the page should run on one comic per week, as it used to five years ago. You know, the internet is a strange place now. People read like fifty pages per minute, and I believe a page that publishes once a week will barely keep the usual readers interested, let alone attract new visitors. Im shoving this page down my friends and families throats. But as previously mentioned, this time Im not trying to break into the market or make my living out of it, so a lot of the pressure is off. This is like blogging about my dogs life, but with pictures. I am so happy and satisfied with all the positive feedback. I am also sooo happy to practice coding, and I am pretty satisfied with the work Ive done here so far. ATM I live in Curitiba, probably the best city in the world. I live in a tiny flat in a huge building, where I met some of the best friends a person could have. I have an amazing day job where I work alongside awesome people (ohhh, and they have a dress code, and its all black and white). Furthermore, I noticed a brutal change on my creative work, and even on my coding assignments. I do not feel like Im rushing to meet deadlines any more, I just feel like doing the things because I want to do them. My schedule is normally organized, and I dont feel like I am constantly letting myself down. I have a suspicion that I owe that to my incredible therapist, who has been with me for the past four years. (Still no paragraphs cause javascript told me so). ",
-  },
-  {
-    issueNumber: 006,
-    issueTitle: "Plump Dog",
-    url: "0006.png",
-    text: "Is it supposed to be a Monday comic? Yes. Do I get overly excited and share it on Sunday? Maybe.",
-  },
-  {
-    issueNumber: 007,
-    issueTitle: "Lazy Dog",
-    url: "0007.png",
-    text: "The dog always wants to go for walks. Unless it's 7am. I am absolutely swamped with work and javascript assignments, and that is why the website hasnt progressed one bit.",
-  },
-];
-const imagesPath = "assets/comics/";
+let isEnglish = true;
+
+let scriptElement = document.createElement("script");
+
+scriptElement.type = "text/javascript";
+
+document.getElementById("explodingSite").appendChild(scriptElement);
+
+function toggleLanguage() {
+  if (isEnglish) {
+    scriptElement.remove(); // Remove the existing script element
+    scriptElement = document.createElement("script"); // Create a new script element
+    scriptElement.src = "js/localEng.js"; // Set the source file for the English version
+    document.getElementById("explodingSite").appendChild(scriptElement);
+    console.log(scriptElement.src);
+  } else {
+    scriptElement.remove(); // Remove the existing script element
+    scriptElement = document.createElement("script"); // Create a new script element
+    scriptElement.src = "js/localPt.js"; // Set the source file for the Portuguese version
+    document.getElementById("explodingSite").appendChild(scriptElement);
+
+  }
+  
+}
+
+toggleLanguage();
 
 let currentImageIndex = 0; //Keeps track of the currently displayed image
 
@@ -56,11 +37,12 @@ const issueNumber = document.getElementById("numberStrip");
 const issueTitle = document.getElementById("nameStrip");
 const imageElement = document.getElementById("imageCarousel");
 const text = document.getElementById("blog-text");
+const topMenuLang = document.getElementById("topText");
 text.textContent = images[currentImageIndex].text;
 issueNumber.textContent = "#" + images[currentImageIndex].issueNumber;
 issueTitle.textContent = images[currentImageIndex].issueTitle;
 //here is the callback or event handler for the event listener:
-firstBtn.addEventListener("click", showFirstImage); 
+firstBtn.addEventListener("click", showFirstImage);
 previousBtn.addEventListener("click", showPreviousImage);
 randomBtn.addEventListener("click", showRandomImage);
 nextBtn.addEventListener("click", showNextImage);
@@ -105,3 +87,11 @@ function updateImageSource() {
   issueNumber.textContent = "#" + images[currentImageIndex].issueNumber;
   issueTitle.textContent = images[currentImageIndex].issueTitle;
 }
+
+firstBtn.textContent = first;
+previousBtn.textContent = previous;
+randomBtn.textContent = random;
+nextBtn.textContent = next;
+lastBtn.textContent = last;
+
+topMenuLang.textContent = topTitle;
